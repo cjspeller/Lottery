160ac858515302a1a3e5d39693a9fe564fa5a38b
Cartrell Speller <cjspeller0211@gmail.com>
2019-07-29 21:53:17 -0400

    modfication to random numbers for lottery to make lottery numbers visiable on the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log('hey im connected');
+// Mega million
+// 5 numbers from 1 to 70 ... will not repeat .. TODO: remember to get rid of repeats
+
+document.querySelector('#number1').innerHTML = Math.floor(Math.random()*70) + 1
+document.querySelector('#number2').innerHTML = Math.floor(Math.random()*70) + 1
+document.querySelector('#number3').innerHTML = Math.floor(Math.random()*70) + 1
+document.querySelector('#number4').innerHTML = Math.floor(Math.random()*70) + 1
+document.querySelector('#number5').innerHTML = Math.floor(Math.random()*70) + 1
+document.querySelector('#number6').innerHTML = Math.floor(Math.random()*70) + 1
+
+
+// 1 mega ball from 1 to 25
+document.querySelector('#megaball').innerHTML = Math.floor(Math.random()*25) + 1
